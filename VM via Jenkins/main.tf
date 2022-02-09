ac8607d56af59b08286c851b2fd1b71aca53da2e
Pithani.Kishan Dev <44198442+kishandev1999@users.noreply.github.com>
2022-02-09 14:03:16 +0530

    Add files via upload

---
+++ VM via Jenkins/main.tf
@@ -0,0 +1,23 @@
+resource "google_compute_instance" "vm_instance" {
+  name         = "sample-vm"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+
+}
